14d901d8693d70b2aa3e51ba29fdb0f06095a0a6
oussama <ohaouach27@gmail.com>
2020-08-17 00:36:14 +0100

    done

--- assignment23.py
+++ assignment23.py
@@ -1,2 +1,11 @@
+class unique_subsets:
+    def sub_sets(self, set):
+        return self.sub_sets_Recur([], sorted(set))
+
+    def sub_sets_Recur(self, current, set):
+        if set:
+            return self.sub_sets_Recur(current, set[1:]) + self.sub_sets_Recur(current + [set[0]], set[1:])
+        return [current]
 
 
+print(unique_subsets().sub_sets([4, 5, 6]))
