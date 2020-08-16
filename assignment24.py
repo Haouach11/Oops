53d8b3ccf0894641470d38a565b6ed0d9c48a5b5
oussama <ohaouach27@gmail.com>
2020-08-17 00:29:41 +0100

    done

--- assignment24.py
+++ assignment24.py
@@ -1,1 +1,14 @@
+class Circle:
+    def __init__(self, r):
+        self.radius = r
 
+    def area(self):
+        return self.radius ** 2 * 3.14
+
+    def perimeter(self):
+        return 2 * self.radius * 3.14
+
+
+Circle1 = Circle(int(input("enter a value: ")))
+print("the area is: " , Circle1.area())
+print("the perimeter is: " , Circle1.perimeter())
