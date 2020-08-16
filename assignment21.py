961941a9054617d8334ac49551d9a1c3e68445f8
oussama <ohaouach27@gmail.com>
2020-08-16 23:40:51 +0100

    done

--- assignment21.py
+++ assignment21.py
@@ -1,1 +1,19 @@
+class Person:
+    def __init__(self,name,age,):
+        self.name=name
+        self.age=age
+    def my_function(self):
+        print("My name is " + self.name)
+        print("I am",end=' ')
+
+
+p1 = Person("oussama", 18)
+p1.my_function()
+p1.age = 18
+print(p1.age)
+
+
+
+
+
 
